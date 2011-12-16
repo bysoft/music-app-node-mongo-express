56ca68c7cbd86f5b24fd2c1c914d5e8eef9b059a
Brian Yang <brianyang1@gmail.com>
2011-12-15 21:38:12 -0500

    connect to spotify with hardcoded query

--- app.js
+++ app.js
@@ -29,7 +29,36 @@ app.configure('production', function(){
 
 // Routes
 
-app.get('/', routes.index);
+// app.get('/', routes.index);
 
+
+app.get('/spotify-query', function(req, res){
+    res.send('spotify query')
+    var spotify = require('spotify');
+
+spotify.search({ type: 'track', query: 'britney spears' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+
+console.dir(data.tracks[0].album.name)
+// console.dir('show data')
+
+    // Do something with 'data'
+});
+
+});
+
+
+
+app.get('/',function(req,res){
+  res.send('check console')
+
+
+})
+
+
+app.get('/spotify',routes.spotify)
 app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+exports.spotify = function(req,res){
+  res.render('spotify',{title:'spotify'})
+}
